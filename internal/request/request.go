05efa4c8f6b2e80908fe1013afb0d8e5f0ca496c
Juan Cruz <cruzjuan298@gmail.com>
2026-01-03 23:41:47 -0500

    add: parse request line logic for incoming request

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,85 @@
 package request
+
+import (
+	"io"
+	"strings"
+	"fmt"
+	"unicode"
+)
+
+type RequestLine struct {
+	HttpVersion string
+	RequestTarget string
+	Method string
+}
+
+type Request struct {
+	RequestLine RequestLine	
+}
+
+func StringIsUpper(s string) bool {
+	for _, char := range s {
+		if !unicode.IsUpper(char) ||  !unicode.IsLetter(char){
+			return false
+		}
+	}
+	return true
+}
+
+func ParseRequestLine(s string) (string, string, string, error) {
+	
+	splitString1 := strings.Split(s, " ")
+
+	if len(splitString1) < 2 {
+		return "", "", "", fmt.Errorf("Request line is missing information")
+	}
+
+	method := splitString1[0]
+
+	if StringIsUpper(method) == false {
+		return "", "", "", fmt.Errorf("Error in the request line parsing: Invalid Method format")
+	}
+
+	requestTarget := splitString1[1]
+
+	if strings.HasPrefix(requestTarget, "/") == false {
+		return "", "", "", fmt.Errorf("Error in the request line parsing: Invalid resource format")
+	}
+
+	versionSplit1 := strings.Split(splitString1[2], "/")
+
+	versionSplit2 := strings.Split(versionSplit1[1], "\r\n")
+
+	httpVersion := versionSplit2[0]
+
+	if httpVersion != "1.1" {
+		return "", "", "", fmt.Errorf("Error in the request line parsing: Unsupported http version")
+	}
+
+	return method, requestTarget, httpVersion, nil
+
+}
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	buffer, err := io.ReadAll(reader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	requestLineString := string(buffer)
+
+	requestMethod, requestTarget, httpVersion, parsingError := ParseRequestLine(requestLineString)
+
+	if parsingError != nil {
+		return nil, parsingError
+	}
+
+	var returnedRequest Request
+
+	returnedRequest.RequestLine.HttpVersion = httpVersion
+	returnedRequest.RequestLine.RequestTarget = requestTarget
+	returnedRequest.RequestLine.Method = requestMethod
+
+	return &returnedRequest, nil
+ 
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -2,9 +2,79 @@ package request
 
 import ( 
 	"testing"
+	"strings"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func request_text (t *testing.T) {
-	assert.Equal(t, "test", "test")  
+func TestParseRequestLine_NoHeaders (t *testing.T) {
+	r, error := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\n\r\n"))
+	require.NoError(t, error)
+	require.NotNil(t, r)
+	assert.Equal(t, "GE", r.RequestLine.Method)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+}
+
+func TestParseRequestLine_WithQueryParams(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(
+		"GET /search?q=test&lang=en HTTP/1.1\r\n\r\n",
+	))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+
+	assert.Equal(t, "/search?q=test&lang=en", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestParseRequestLine_HTTP10(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(
+		"GET /legacy HTTP/1.0\r\n\r\n",
+	))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+
+	assert.Equal(t, "/legacy", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.0", r.RequestLine.HttpVersion)
+}
+
+func TestParseRequestLine_ExtraSpaces(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(
+		"GET    /foo/bar    HTTP/1.1\r\n\r\n",
+	))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+
+	assert.Equal(t, "/foo/bar", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestParseRequestLine_MissingHTTPVersion(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(
+		"GET /\r\n\r\n",
+	))
+	require.Error(t, err)
+	assert.Nil(t, r)
+}
+
+func TestParseRequestLine_InvalidHTTPVersion(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(
+		"GET / HTTP/2\r\n\r\n",
+	))
+	require.Error(t, err)
+	assert.Nil(t, r)
+}
+
+func TestParseRequestLine_EmptyInput(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(""))
+	require.Error(t, err)
+	assert.Nil(t, r)
+}
+
+func TestParseRequestLine_TooManyTokens(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader(
+		"GET / HTTP/1.1 EXTRA\r\n\r\n",
+	))
+	require.Error(t, err)
+	assert.Nil(t, r)
 }
