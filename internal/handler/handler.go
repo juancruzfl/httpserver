a37a31f217dabb832338f78a18d39c8dab231ff8
Juan <cruzjuan298@gmail.com>
2026-01-28 04:08:39 -0500

    feat: implement server multiplexer(router) and handle methods

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,10 +5,14 @@ import (
 	"github.com/juancruzfl/httpserver/internal/response"
 )
 
+// as we can see here, a handlder is specified via this function signature. The response writer is what is being used to actually send a response back to the client. The request is the structure 
+// that holds the already parsed information, which was done within our server. We are quite literally 'handling' a request.
 type Handler interface {
 	ServeHttp(response.CustomResponseWriter, *request.Request)
 }
 
+// sidenote: this is a function type that is meant to be the adpater to the handler (above) interface. We do this because we can't 
+// attach interfaces to methods. We need some concrete type, so we use a function type to accomplish this.
 type HandlerFunc func(response.CustomResponseWriter, *request.Request)
 
 func (h HandlerFunc) ServerHttp(w response.CustomResponseWriter, r *request.Request) {
--- internal/server/server.go
+++ internal/server/server.go
@@ -7,6 +7,29 @@ import (
 	"github.com/juancruzfl/httpserver/internal/handler"
 )
 
+// sidenote: this is what is considered a 'router' in most http frameworks. In simple terms it helps the http server keep track of routes and their associated handlers
+type MyServerMux struct {
+	routes map[string]handler.Handler
+}
+
+func NewServerMux() *MyServerMux {
+	return &MyServerMux{
+		routes: map[string]handler.Handler{},
+	}
+}
+
+// sidenote: we are casting the function that is being pass to the HandleFunc method to the HandlerFunc apdater. It in turn, gives
+// us a valid handler, which is useful since we can reuse our Handle method.
+func (m *MyServerMux) HandleFunc(route string, f func(w response.CustomResponseWriter, r *request.Request) {
+	m.Handle(route, handler.HanlderFunc(f))
+}
+
+func (m *MyServerMux) Handle(route string, handler handler.Handler) {
+	if m.routes == nil {
+		m.routes = map[string]handler.Handler{}
+	}
+	m.routes[route] = handler
+}
 
 func CustomListenAndServe(addr string, h handler.Handler) (
 	listener, err := net.Listen("tcp", addr)
